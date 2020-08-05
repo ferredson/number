d56859267e6cecadab5e4be579356f40037ed35a
ferredson <69278114+ferredson@users.noreply.github.com>
2020-08-05 20:19:53 -0300

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+ /* Trabalho desenvolvido para Repositório do GitHub da disciplina AED3 */ 
+
+#include <iostream>
+using namespace std;
+int main ()
+
+{ 
+ int n1, n2, soma; 
+ cout << "\n Insira N1 = " ; 
+ cin >> n1 ; 
+ cout << "\n Insira N2 = " ; 
+ cin >> n2 ; 
+ soma = n1 + n2; 
+ cout << "\n A soma de "<< n1 <<" e "<< n2 <<" = "<< soma; 
+ return 0 ; 
+}
+
